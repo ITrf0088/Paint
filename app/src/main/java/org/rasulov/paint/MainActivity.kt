28c1c54fabd3632e4802883df4817121b2d7d85c
ITrf0088 <itrf0088@gmail.com>
2022-04-25 21:06:39 +0500

    Selecting the Color was created

--- app/src/main/java/org/rasulov/paint/MainActivity.kt
+++ app/src/main/java/org/rasulov/paint/MainActivity.kt
@@ -1,33 +1,63 @@
 package org.rasulov.paint
 
 import android.app.Dialog
-import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
-import android.graphics.drawable.ShapeDrawable
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.DialogCompat
 import androidx.core.content.ContextCompat
+import androidx.core.view.children
 import org.rasulov.paint.drawingview.Drawing
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var drawing: Drawing
+    private lateinit var linearPallet: LinearLayout
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawing = findViewById(R.id.drawing)
+        linearPallet = findViewById(R.id.linear_pallet)
         val imgBtn = findViewById<ImageButton>(R.id.img_btn_select_b_size)
-        imgBtn.setOnClickListener { showDialog() }
 
+        imgBtn.setOnClickListener { showDialog() }
+        setPalette()
+        setListeners()
+
+    }
+
+    private fun setListeners() {
+        linearPallet.children.forEach { it ->
+            it.setOnClickListener {
+                drawing.color = Color.parseColor(it.tag.toString())
+                setPalette()
+            }
+        }
+    }
+
+    private fun setPalette() {
+        linearPallet.children.forEach {
+            val parseColor = Color.parseColor(it.tag.toString())
+            var drawable = ContextCompat.getDrawable(this, R.drawable.pallet_normal)?.mutate()
+            if (parseColor == drawing.color)
+                drawable = ContextCompat.getDrawable(this, R.drawable.pallet_selected)
+
+            if (drawable is GradientDrawable) {
+                drawable.setColor(parseColor)
+                it.background = drawable
+            }
+
+        }
     }
 
 
     private fun showDialog() {
-
         val dialog = Dialog(this)
         dialog.setContentView(R.layout.dialog_brush_size)
         val small = DialogCompat.requireViewById(dialog, R.id.img_btn_small_b_size)
@@ -36,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         setUpImgBtn(dialog, small, 5f)
         setUpImgBtn(dialog, medium, 10f)
         setUpImgBtn(dialog, large, 15f)
-
         dialog.show()
     }
 
@@ -50,3 +79,5 @@ class MainActivity : AppCompatActivity() {
     }
 
 }
+
+
